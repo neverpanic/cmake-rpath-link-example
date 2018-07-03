1df63033a2460b72d813747efadebe38218058b8
Clemens Lang <clemens.lang@bmw-carit.de>
2018-07-03 23:53:53 +0200

    Also build in the trigger script

--- trigger.sh
+++ trigger.sh
@@ -2,4 +2,4 @@
 set -e
 
 rm -rf build && mkdir -p build
-cd build && cmake -GNinja -DCMAKE_CXX_FLAGS="--sysroot=/nonexistant" ..
+cd build && cmake -GNinja -DCMAKE_CXX_FLAGS="--sysroot=/nonexistant" .. && ninja -v
