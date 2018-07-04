7943c24cf31c80d7ccd70fe84af93e823f057944
Clemens Lang <clemens.lang@bmw-carit.de>
2018-07-04 16:11:35 +0200

    Use CMAKE_SYSROOT to set the sysroot
    Make sure we're doing things the CMake way, rather than hand-rolling the
    --sysroot argument.

--- trigger.sh
+++ trigger.sh
@@ -2,4 +2,4 @@
 set -e
 
 rm -rf build && mkdir -p build
-cd build && cmake -GNinja -DCMAKE_CXX_FLAGS="--sysroot=/nonexistant" .. && ninja -v
+cd build && cmake -GNinja -DCMAKE_TOOLCHAIN_FILE="../toolchain.cmake" .. && ninja -v
